1ab4eaf2b60169343b36ca17d2ef110034daece3
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2023-02-07 13:01:59 -0300

    arrays-slice

---
+++ arrays-slice.js
@@ -0,0 +1,6 @@
+const time = ["Pamella", "Ana Viterale", "Izabella", "Leticia", "Ana Carla", "Jenifer", "Marcela", "Wania", "Marcia", "Vagner", "Samuel", "Solange", "Jessyca", "Priscila" ]
+
+time1 = time.slice(0, time.length / 2)
+time2 = time.slice(time.length / 2)
+console.log(time1)
+console.log(time2)
