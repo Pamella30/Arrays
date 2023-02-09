2ef7fe383bb68973fd1d7d674d17e65021a72d8c
P.Cruz <74403459+Pamella30@users.noreply.github.com>
2023-02-09 17:55:35 -0300

    update

---
+++ arrays-for-classico.js
@@ -0,0 +1,9 @@
+const numeros = [100, 200, 300, 400]
+
+for (let indice = 0; indice < numeros.length; indice++) {
+    console.log(numeros[indice])
+}
+
+// Primeiro parametro foi criado uma variavel que e executada uma unica vez
+// Segundo parametro e a condicao de execucao
+// Terceiro parametro e executado no final fazendo o incremento
